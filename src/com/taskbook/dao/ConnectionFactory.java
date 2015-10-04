e1bd184c4c362ed820fe1b296977646a8b3dbe07
rroshan <r_roshan@hotmail.com>
2015-10-04 18:50:19 -0500

    Started DAO Coding
    Created Singleton ConnectionFactory Class
    Added viewTasklist method to TaskbookDAO
    Added a TestServlet for testing DAO working.
    Added test.jsp to display results retrieved from the DAO.

---
+++ src/com/taskbook/dao/ConnectionFactory.java
@@ -0,0 +1,69 @@
+package com.taskbook.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/*
+ * Singleton Pattern
+ * Eager Initialization of ConnectionFactory.
+ */
+
+public class ConnectionFactory {
+	private static final ConnectionFactory instance = new ConnectionFactory();
+	private static DataSource dataSource;
+
+	private ConnectionFactory() {
+		try {
+			InitialContext context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/TestDB");
+
+		} catch (NamingException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private Connection createConnection() {
+		Connection conn = null;
+		try {
+			conn = dataSource.getConnection();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return conn;
+	}   
+
+	public static Connection getConnection() {
+		return instance.createConnection();
+	}
+
+	public static void closeResources(ResultSet set, Statement statement, Connection conn) {
+		if (set != null) {
+			try {
+				set.close();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+}
--- src/com/taskbook/dao/TaskbookDAO.java
+++ src/com/taskbook/dao/TaskbookDAO.java
@@ -1,5 +1,56 @@
 package com.taskbook.dao;
 
-public class TaskbookDAO {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import com.taskbook.bo.*;
+
+public class TaskbookDAO {
+	private Connection conn;
+	private Statement stmt;
+	private ResultSet set;
+
+	public TaskbookDAO() {	}
+
+	//modify the return type to an array of tasklists
+	public ArrayList<Tasklist> viewTasklist() {
+
+		ArrayList<Tasklist> arrTasklist = new ArrayList<Tasklist>();
+		Tasklist taskList;
+		
+		//getting database connection from connection pool
+		//connection handled by tomcat
+		conn = ConnectionFactory.getConnection();
+
+		try {
+			stmt = conn.createStatement();
+			String sql = "SELECT tasklist_id, category, owner FROM tasklist";
+			set = stmt.executeQuery(sql);
+
+			while(set.next()){
+				//Retrieve by column name
+				int tasklist_id  = set.getInt("tasklist_id");
+				String category = set.getString("category");
+				String owner = set.getString("owner");
+				
+				taskList = new Tasklist();
+				taskList.setOwner(owner);
+				taskList.setTasklistID(tasklist_id);
+				taskList.setTaskName(category);
+				
+				arrTasklist.add(taskList);
+			}
+		} catch(SQLException sqlex) {
+			sqlex.printStackTrace();
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			ConnectionFactory.closeResources(set, stmt, conn);
+		}
+		
+		return arrTasklist;
+	}
 }
